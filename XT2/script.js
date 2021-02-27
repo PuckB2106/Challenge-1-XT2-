0fb322eda47913137355cde6fc39a8278d72ef87
PuckB2106 <78908678+PuckB2106@users.noreply.github.com>
2021-02-27 17:47:53 +0100

    js

--- XT2/script.js
+++ XT2/script.js
@@ -1,16 +1,121 @@
 var myChart1 = document.getElementById('myChart').getContext('2d');
 var chart1 = new Chart(myChart1, {
     // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['Water',],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'blue',
+            borderColor: 'white',
+            data: [75,]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+
+});
+
+var myChart2 = document.getElementById('myChart2').getContext('2d');
+var chart2 = new Chart(myChart2, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['Water',],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'blue',
+            borderColor: 'white',
+            data: [75,]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+
+});
+
+var myChart3 = document.getElementById('myChart3').getContext('2d');
+var chart3 = new Chart(myChart3, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['Water',],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'blue',
+            borderColor: 'white',
+            data: [75,]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+
+});
+
+var myChart4 = document.getElementById('myChart4').getContext('2d');
+var chart4 = new Chart(myChart4, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['Water',],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'blue',
+            borderColor: 'white',
+            data: [75,]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+
+});
+
+var myChart5 = document.getElementById('myChart5').getContext('2d');
+var chart5 = new Chart(myChart5, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['Water',],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'blue',
+            borderColor: 'white',
+            data: [75,]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+
+});
+
+var myChart6 = document.getElementById('myChart6').getContext('2d');
+var chart6 = new Chart(myChart6, {
+    // The type of chart we want to create
     type: 'pie',
 
     // The data for our dataset
     data: {
-        labels: ['Water', 'Frisdrank',],
+        labels: ['Water',],
         datasets: [{
             label: 'My First dataset',
-            backgroundColor: '',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
+            backgroundColor: 'blue',
+            borderColor: 'white',
+            data: [75,]
         }]
     },
 
