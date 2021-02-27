2e009666ced56cb162559ed272acd3d8696c96e0
PuckB2106 <78908678+PuckB2106@users.noreply.github.com>
2021-02-28 00:15:16 +0100

    Lettertype
    Lettertype toegevoegd

--- XT2/script.js
+++ XT2/script.js
@@ -1,44 +1,4 @@
-var myChart1 = document.getElementById('myChart').getContext('2d');
-var chart1 = new Chart(myChart1, {
-    // The type of chart we want to create
-    type: 'line',
 
-    // The data for our dataset
-    data: {
-        labels: ['Water',],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'blue',
-            borderColor: 'white',
-            data: [75,]
-        }]
-    },
-
-    // Configuration options go here
-    options: {}
-
-});
-
-var myChart2 = document.getElementById('myChart2').getContext('2d');
-var chart2 = new Chart(myChart2, {
-    // The type of chart we want to create
-    type: 'line',
-
-    // The data for our dataset
-    data: {
-        labels: ['Water',],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'blue',
-            borderColor: 'white',
-            data: [75,]
-        }]
-    },
-
-    // Configuration options go here
-    options: {}
-
-});
 
 var myChart3 = document.getElementById('myChart3').getContext('2d');
 var chart3 = new Chart(myChart3, {
