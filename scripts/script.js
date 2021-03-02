20a959054101f6466496b984bcc1f81c977525ab
PuckB2106 <78908678+PuckB2106@users.noreply.github.com>
2021-03-02 13:50:50 +0100

    bestanden verplaatsing

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-
 var myChart1 = document.getElementById('myChart1').getContext('2d');
 var chart1 = new Chart(myChart1, {
     // The type of chart we want to create
@@ -17,13 +16,13 @@ var chart1 = new Chart(myChart1, {
     },
 
     // Configuration options go here
-    options: {responsive:true,
-      legend:{
-             display:false
-           },
-
+    options: {
+        responsive: true,
+        legend: {
+            display: false
+        },
+        maintainAspectRatio: false,
     }
-
 });
 
 var myChart2 = document.getElementById('myChart2').getContext('2d');
@@ -44,12 +43,13 @@ var chart2 = new Chart(myChart2, {
     },
 
     // Configuration options go here
-    options: {responsive:true,
-      legend:{
-             display:false
-           },
+    options: {
+        responsive: true,
+        legend: {
+            display: false
+        },
+        maintainAspectRatio: false,
     }
-
 });
 
 
@@ -60,19 +60,21 @@ var chart3 = new Chart(myChart3, {
 
     // The data for our dataset
     data: {
-        labels: ['Waterstof','Ethanol','Benzine','Hydrazine','Methaan','Propeen','Zuurstof'],
+        labels: ['Waterstof', 'Ethanol', 'Benzine', 'Hydrazine', 'Methaan', 'Propeen', 'Zuurstof'],
         datasets: [{
             label: ['Hoeveelheid brandstof'],
-            backgroundColor: ['#00538C','#ff006e','#009ADB','#FFE700','#fb5607','#8338ec','#80b918',],
-            data: [25,99,75,30,80,59,90,]
+            backgroundColor: ['#00538C', '#ff006e', '#009ADB', '#FFE700', '#fb5607', '#8338ec', '#80b918',],
+            data: [25, 99, 75, 30, 80, 59, 90,]
         }]
     },
 
     // Configuration options go here
-    options: {responsive:true,
-      legend:{
-       display:false,
-     },
+    options: {
+        responsive: true,
+        legend: {
+            display: false,
+        },
+        maintainAspectRatio: false,
     }
 
 });
@@ -84,7 +86,7 @@ var chart4 = new Chart(myChart4, {
 
     // The data for our dataset
     data: {
-        labels: ['0','5','10','15','20','25','30','35','40','45','50',],
+        labels: ['0', '5', '10', '15', '20', '25', '30', '35', '40', '45', '50',],
         datasets: [{
             label: 'My First dataset',
             backgroundColor: '#051C2B',
@@ -94,36 +96,41 @@ var chart4 = new Chart(myChart4, {
     },
 
     // Configuration options go here
-    options: {responsive:true,
-      legend:{
-       display:false,
-     },}
+    options: {
+        responsive: true,
+        legend: {
+            display: false,
+        },
+        maintainAspectRatio: false,
+    }
 
 });
-
 
 
 var myChart5 = document.getElementById('myChart5').getContext('2d');
 var chart5 = new Chart(myChart5, {
     // The type of chart we want to create
-    type: 'pie',
+    type: 'line',
 
     // The data for our dataset
     data: {
-        labels: ['20','40','10','15','5','7','8',],
+        labels: ['0', '5', '10', '15', '20', '25', '30', '35', '40', '45', '50',],
         datasets: [{
-            label: [],
-            backgroundColor: ['#f72585','#b5179e','#7209b7','#560bad','#3f37c9','#4895ef','#4cc9f0',],
-            borderColor: ['white',],
-            data: ['20','40','10','15','5','7','8',]
+            label: 'My First dataset',
+            backgroundColor: '#051C2B',
+            borderColor: 'white',
+            data: [0, 1, 1.5, 3, 2, 3.5, 6.1, 5, 6, 4, 5,]
         }]
     },
 
     // Configuration options go here
-    options: {responsive:true,
-      legend:{
-       display:false,
-     },}
+    options: {
+        responsive: true,
+        legend: {
+            display: false,
+        },
+        maintainAspectRatio: false,
+    }
 
 });
 
@@ -139,11 +146,14 @@ var chart6 = new Chart(myChart6, {
             label: 'My First dataset',
             backgroundColor: '#00365A',
             borderColor: 'white',
-            data: [75,100,]
+            data: [75, 100,]
         }]
     },
 
     // Configuration options go here
-    options: {responsive:true}
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+    }
 
 });
